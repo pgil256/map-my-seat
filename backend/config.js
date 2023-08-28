74a95db2a51932401fa432012c6881f35cada2df
Patrick Gilhooley <113308245+pgil256@users.noreply.github.com>
2023-08-28 18:44:45 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,9 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const PORT = +process.env.PORT || 3001;
+const HOSTNAME = process.env.HOSTNAME || (process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost");
+
+const PORT = +process.env.PORT || 3000;
 
 //Automates URI to correct db URI based on whether in testing env or not
 function getDatabaseUri() {
